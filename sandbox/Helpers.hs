eef4157ed0a7d9e066e5eb31aa264591c34d5620
geo2a <georgiylukjanov@gmail.com>
2014-11-25 11:07:02 +0300

    Парсеры many и many1 заменены на функции many и some из класса Alternative

--- sandbox/Helpers.hs
+++ sandbox/Helpers.hs
@@ -1,5 +1,3 @@
 module Helpers where
 
 wspaceOrTab = (\x -> x == ' ' || x == '\t')
-
-isSpace = (\x -> x == ' ' || x == '\n' || x == '\t')
--- sandbox/Parsers.hs
+++ sandbox/Parsers.hs
@@ -1,7 +1,8 @@
 module Parsers where
 
 import Control.Monad
-import Data.Char(ord)
+import Control.Applicative
+import Data.Char
 
 import Helpers
 
@@ -19,8 +20,8 @@ instance Monad Parser where
 instance MonadPlus Parser where
   mzero = Parser (\cs -> [])
   p `mplus` q = Parser (\cs -> 
-    let ps = parse p cs if 
-      if bull ps then parse q cs else ps)
+    let ps = parse p cs in 
+      if null ps then parse q cs else ps)
 
 instance Functor Parser where
   fmap = liftM
@@ -35,7 +36,7 @@ instance Alternative Parser where
 
 -- |Consumes one symbol of any kind 
 item :: Parser Char
-item = Parser (\inp -> case inp of
+item =  Parser (\inp -> case inp of
                         [] -> []
                         (x:xs) -> [(x,xs)])
 
@@ -43,7 +44,8 @@ item = Parser (\inp -> case inp of
 sat :: (Char -> Bool) -> Parser Char
 sat p = do
   x <- item 
-  if p x then return x else mzero
+  guard $ p x
+  return x
 
 ----------------Парсеры для одиночных символов----------------
 
@@ -53,65 +55,43 @@ char x = sat (\y -> x == y)
 
 -- |Decimal digit
 digit :: Parser Char
-digit = sat (\x -> '0' <= x && x <= '9')
+digit = sat isDigit
 
 -- |Lowercase letter
 lower :: Parser Char
-lower = sat (\x -> 'a' <= x && x <= 'z')
+lower = sat isLower
 
 -- |Uppercase letter
 upper :: Parser Char
-upper = sat (\x -> 'A' <= x && x <= 'Z')
+upper = sat isUpper
 
 -- |Anycase letter
 letter :: Parser Char
-letter = lower `mplus` upper
+letter = lower <|> upper
 
 -- |Anycase letter or decimal digit
 alphanum :: Parser Char
-alphanum = letter `mplus` digit
+alphanum = letter <|> digit
 
 newline :: Parser Char
 newline  = char '\n'  
 ----------------Парсеры для групп символов----------------
 
--- |Word (non-empty string of letters)
-word :: Parser String
-word = do
-  l <- letter
-  ls <- word'
-  return $ l:ls
-
--- |Same as word, but string may be empty 
-word' = nonEmpty `mplus` return ""
-  where
-    nonEmpty = do
-      x <- letter
-      xs <- word'
-      return (x:xs)
-
--- |Applyes parser p zero or more times
-many :: Parser a -> Parser [a]
-many p = many1 p <|> many0
-  where
-    many0 = return []
-
-many1 :: Parser a -> Parser [a]
-many1 p = do 
-  a <- p
-  as <- many p
-  return (a:as)
+-- ОФИГЕТЬ!! Добавив инстанс для Alternative, 
+-- мы даром получаем many == Alternative.many и 
+-- many1 == Alternative.some 
 
 -- |Parse a specified string
 string :: String -> Parser String
-string = mapM char -- sequence . map char
+string = mapM char
 
--- |Parse a token with specific parser, thow away any trailing spaces
+-- |Word (non-empty string of letters)
+word :: Parser String 
+word = some letter 
+
+-- |Parse a token with specific parser, throw away any trailing spaces
 token :: Parser a -> Parser a
-token p = do 
-  a <- p 
-  spaces
-  return a
+token p = spaces >> p
 
 -- |Parse a symbolic token, just a specification of token parser
 symbol :: String -> Parser String
