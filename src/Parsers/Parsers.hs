cc42c4e12438f06db20927ba6ed01aa12a67e784
Georgiy Lukjanov <georgiylukjanov@gmail.com>
2015-01-17 23:34:27 +0300

    Все базовые парсеры обобщены до TextualMonoid!

--- src/Parsers/Parsers.hs
+++ src/Parsers/Parsers.hs
@@ -6,7 +6,8 @@ import Control.Monad
 import Data.Maybe (maybeToList)
 import Data.Monoid (mempty, mappend, mconcat)
 import qualified Data.Monoid.Textual as TM
-import Data.Monoid.Factorial (splitAt)
+--import Data.Monoid.Factorial (splitAt)
+import Data.Char
 import Data.Text (pack) -- для теста совместимости
 
 --import Helpers
@@ -59,48 +60,50 @@ char :: TM.TextualMonoid t => Char -> Parser t Char
 char x = sat (\y -> x == y)
 
 ---- |Decimal digit
---digit :: Parser Char
---digit = sat isDigit
+digit :: TM.TextualMonoid t => Parser t Char
+digit = sat isDigit
 
----- |Lowercase letter
---lower :: Parser Char
---lower = sat isLower
+-- |Lowercase letter
+lower :: TM.TextualMonoid t => Parser t Char
+lower = sat isLower
 
----- |Uppercase letter
---upper :: Parser Char
---upper = sat isUpper
+-- |Uppercase letter
+upper :: TM.TextualMonoid t => Parser t Char
+upper = sat isUpper
 
----- |Anycase letter
---letter :: Parser Char
---letter = lower <|> upper
+-- |Anycase letter
+letter :: TM.TextualMonoid t => Parser t Char
+letter = lower <|> upper
 
----- |Anycase letter or decimal digit
---alphanum :: Parser Char
---alphanum = letter <|> digit
+-- |Anycase letter or decimal digit
+alphanum :: TM.TextualMonoid t => Parser t Char
+alphanum = letter <|> digit
 
---newline :: Parser Char
---newline  = char '\n'  
+newline :: TM.TextualMonoid t => Parser t Char
+newline  = char '\n'  
 ------------------Парсеры для групп символов----------------
 
 ---- ОФИГЕТЬ!! Добавив инстанс для Alternative, 
 ---- мы даром получаем many == Alternative.many и 
 ---- many1 == Alternative.some 
 
----- |Parse a specified string
---string :: String -> Parser String
---string = mapM char
+-- TODO: подумать, что делать с возвращаемым значение следующего парсера
+-- стоит оставить String, или переделать на t? 
+-- |Parse a specified string
+string :: TM.TextualMonoid t => String -> Parser t String
+string = mapM char
 
----- |Word (non-empty string of letters)
---word :: Parser String 
---word = some letter 
+-- |Word (non-empty string of letters)
+word :: TM.TextualMonoid t => Parser t String 
+word = some letter 
 
----- |Parse a token with specific parser, throw away any trailing spaces
---token :: Parser a -> Parser a
---token p = spaces >> p
+-- |Parse a token with specific parser, throw away any trailing spaces
+token :: TM.TextualMonoid t => Parser t a -> Parser t a
+token p = spaces >> p
 
----- |Parse a symbolic token, just a specification of token parser
---symbol :: String -> Parser String
---symbol cs = token (string cs)
+-- |Parse a symbolic token, just a specification of token parser
+symbol :: TM.TextualMonoid t => String -> Parser t String
+symbol cs = token (string cs)
 
 ---- TODO: Это, похоже, не востребовано
 ---- |Parse identifier, i.e. word with leading lowercase letters and trailing alphanums 
@@ -117,29 +120,29 @@ char x = sat (\y -> x == y)
 --  x <- ident
 --  if elem x kwords then return "" else return x
 
----- |Parse a thing enclosed by brackets
---bracket :: Parser a -> Parser b -> Parser c -> Parser b
---bracket open p close = do 
---  open
---  x <- p
---  close
---  return x
+-- |Parse a thing enclosed by brackets
+bracket :: TM.TextualMonoid t => Parser t a -> Parser t b -> Parser t c -> Parser t b
+bracket open p close = do 
+  open
+  x <- p
+  close
+  return x
 
 ------------------"Lexical issues"----------------
---spaces :: Parser String
---spaces = many (sat isSpace)
+spaces :: TM.TextualMonoid t => Parser t String
+spaces = many (sat isSpace)
 
 ------------------Repetitions with seporators----------------
 ---- TODO: parse (word `sepby1` spaces) "aaa@aaa" == [(["aaa"],"@aaa")]
 ---- Разве не должен парсер sepby1 фейлится в таком случае? Разобраться.
 ---- Может быть, стоит добавить что-то вроде: guard $ null as?   
 
---sepby :: Parser a -> Parser b -> Parser [a]
---p `sepby` sep = (p `sepby1` sep) <|> return []
+sepby :: TM.TextualMonoid t => Parser t a -> Parser t b -> Parser t [a]
+p `sepby` sep = (p `sepby1` sep) <|> return []
 
---sepby1 :: Parser a -> Parser b -> Parser [a]
---p `sepby1` sep = do 
---  a <- p
---  as <- many (sep >> p)
---  return (a:as)
---  -- or this: (:) <$> p <∗> many (sep >> p)
+sepby1 :: TM.TextualMonoid t => Parser t a -> Parser t b -> Parser t [a]
+p `sepby1` sep = do 
+  a <- p
+  as <- many (sep >> p)
+  return (a:as)
+  -- or this: (:) <$> p <∗> many (sep >> p)
